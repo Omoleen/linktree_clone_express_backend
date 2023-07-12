810f3000fc7f036779c99d90e340e03abba3a356
omoleen <Oreoluwa123>
2023-07-12 21:27:58 +0100

    mongodb atlas

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/Linktree')
+const mongodb_url = process.env.DEBUG === "false" ? process.env.PROD_MONGODB_URL : process.env.DEV_MONGODB_URL
+mongoose.connect(mongodb_url)
     .then(() => console.log('DB connected'))
     .catch(err => console.log(err))
